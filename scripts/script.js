42410ee333fb921ddfffa6e86538c4daab46d42a
Typoonja1 <joaopedro.martisnespindola@gmail.com>
2022-11-08 02:42:12 -0300

    Login

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const formEl = document.querySelector('.form');
+
 document.querySelector("#Show-Login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active");
 });
@@ -5,3 +7,33 @@ document.querySelector("#Show-Login").addEventListener("click",function(){
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
 });
+
+
+
+formEl.addEventListener('submit', event =>{
+    event.preventDefault();
+
+    const formData = new FormData(formEl);
+    const data = new URLSearchParams(formData);
+    fetch('https://reqres.in/api/login',{
+        method: 'Post',
+        body: data
+    }).then(function(Response){
+        console.log(Response.status);
+        if(Response.ok){
+            localStorage.setItem("acesso", true);
+
+            document.querySelector(".popup .logado").addEventListener("click",async function(){
+                document.querySelector(".popup").classList.remove("active");
+            });
+            
+          }else{
+            alert("tenta de novo amigão");
+          }
+          return Response.json();
+      })
+      .catch(error => console.log(error));
+     
+      
+});
+
